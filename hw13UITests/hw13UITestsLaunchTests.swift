a7993f2f8d0ff117241b34250fdf4ee8380cf59d
wair21 <wair21@gmail.com>
2022-08-25 08:49:57 +0300

    Initial Commit

---
+++ hw13UITests/hw13UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  hw13UITestsLaunchTests.swift
+//  hw13UITests
+//
+//  Created by айрат хузиахметов on 25.08.2022.
+//
+
+import XCTest
+
+class hw13UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
